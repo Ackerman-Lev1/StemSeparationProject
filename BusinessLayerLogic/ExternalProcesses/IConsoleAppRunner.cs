aa17a73fcc38389dab00ccb931513e40b58e1f91
SUKUMAR SARIPALLI <sukumarsaripalli@gmail.com>
2025-06-15 22:10:02 +0530

    changes for the login controller and new controller for the spleeter media input

--- BusinessLayerLogic/ExternalProcesses/IConsoleAppRunner.cs
+++ BusinessLayerLogic/ExternalProcesses/IConsoleAppRunner.cs
@@ -11,6 +11,6 @@ namespace BusinessLayerLogic.ExternalProcesses
     {
         //void OpenCommandPrompt();
         string SpleeterArguments(int noOfStems);
-        string RunSpleeter(int noOfStems);
+        Task<string> RunSpleeter(int noOfStems, string originalTrackFilePath);
     }
 }
---
+++ STEMSeperation/Controllers/MediaFileController.cs
@@ -0,0 +1,31 @@
+﻿using BusinessLayerLogic.ExternalProcesses;
+using DatabaseLayerLogic.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using PresentationLayer.ViewModels;
+
+namespace PresentationLayer.Controllers
+{
+    public class MediaFileController : ControllerBase
+    {
+        private readonly IConsoleAppRunner _consoleAppRunner;
+        private readonly IUserRepository _userRepository;
+        public MediaFileController(IConsoleAppRunner consoleAppRunner, IUserRepository userRepository)
+        {
+            _consoleAppRunner = consoleAppRunner;
+            _userRepository = userRepository;
+        }
+        [HttpPost("Upload-Audio")]
+        [Route("CommandCalling")]
+        [Consumes("multipart/form-data")]
+        public async Task<ActionResult> InsertFileAndCallSpleeter(MediaFileVM mediaFileVM)
+        {
+            //this is the part wehre we need to add the new folder creation for the specific user code
+            var originalTrackFilePath = "C:\\Users\\sukum\\Projects\\K_For_Krishna.mp3";
+            //Testing is pending for the below code
+            var fileExecutionResult = await _consoleAppRunner.RunSpleeter(mediaFileVM.noOfStems, originalTrackFilePath);
+            return Ok(fileExecutionResult);
+
+        }
+
+    }
+}
